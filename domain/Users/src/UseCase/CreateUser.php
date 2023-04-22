710c262aa29d672da14497339c6308938ef2daa2
ericzou <amanieric.zou@outlook.com>
2023-04-22 04:06:44 +0000

    Exception User

--- domain/Users/src/UseCase/CreateUser.php
+++ domain/Users/src/UseCase/CreateUser.php
@@ -15,8 +15,25 @@ class CreateUser{
    }
 
     public function execute(array $userData) :?Users{
-        $user = new Users($userData['nom'],$userData['email'],$userData['password'],$userData['createdAt']);
-        $this->userRepository->save($user);
-        return $user;
+        $user = new Users(
+          $userData['nom'] ?? '',
+          $userData['email'] ?? '',
+          $userData['password'] ?? '',
+          $userData['createdAt'] ?? null
+        );
+       try{
+           $this->validate($user);
+           $this->userRepository->save($user);
+           return $user;
+       }catch(LazyAssertionException $e){
+           throw new InvalidUserDataException($e->getMessage());
+       }
+        
     }
+  protected function validate(Users $user){
+    lazy()->that($user->nom)->notBlank()->minLength(3)
+          ->that($user->email)->notBlank()
+          ->that($user->createdAt)->nullOr()->isInstanceOf(DateTimeInterface::class)
+          ->verifyNow();
+  }
 }
