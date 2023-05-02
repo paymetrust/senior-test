a18838737ad6e6a3fe46fd682d3f74a6a006c3c9
ericzou <amanieric.zou@outlook.com>
2023-05-02 01:52:27 +0000

    Configuration de la connection PDO a postgreSSQL

--- infrastructures/Pdo/DataBaseMysql.php
+++ infrastructures/Pdo/DataBaseMysql.php
@@ -10,28 +10,19 @@ class DataBase{
     protected string $dataBase='PostgreSQLdb';
     protected string $user='quitus';
     protected string $password='admin';
-    protected string $port='5432';
+    protected string $port;
     protected string $charset='utf8';
     protected PDO $pdo;
 
     public function getConnexion(){
         $conn = null;
- /*       try {
+        try {
             $conn = new PDO("mysql:host=$this->localhost;dbname=$this->dataBase;charset=$this->charset;",$this->user,$this->password,[
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]);
         } catch (\PDOException $e) {
             echo "Erreur de connexion : ".$e->getMessage();
         }
-       
-   */
-        try{
-            $dsn = "pgsql:host=$this->localhost;port=$this->port;dbname=$this->dataBase;user=$this->dataBase;password=$this->password";
-            $conn = new PDO($dsn);
- 
-        }catch (\PDOException $e){
-             echo "Erreur de connexion : ".$e->getMessage();
-        } 
         return $conn;
     }
 
